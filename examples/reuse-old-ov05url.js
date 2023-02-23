0bad94e6ee14d5ae9a6a25f78ffdc77cb462633e
komori-flag <komoriwork@qq.com>
2023-02-23 11:46:19 +0800

    更新文件

--- examples/reuse-old-ov05url.js
+++ examples/reuse-old-ov05url.js
@@ -1,11 +1,11 @@
 /* 用户配置信息 ============================= */
 
 // debug 信息显示开关
-const debugInfoShow = false; // true：开启，false：关闭
-// 旧直播流地址复用开关
-const oldUrlSwitch = false; // true：开启，false：关闭
-// 传入直播间可录画质检测，如果关闭，则锁定为“原画”画质进行录制
-const optionalQnCheckSwitch = false; // true：开启，false：关闭
+const debugInfoShow = true; // true：开启，false：关闭
+// 旧直播流地址复用开关。如果关闭，则获取到的直播流地址将直接输出而不做暂存
+const oldUrlSwitch = true; // true：开启，false：关闭
+// 传入直播间可录画质检测开关。如果关闭，则锁定为“原画”画质进行录制
+const optionalQnCheckSwitch = true; // true：开启，false：关闭
 // 获取直播流地址的API
 const FETCH_DOMAIN = "https://api.live.bilibili.com"; // "http(s)://域名(:端口号)"
 // 用户登录信息 Cookie（提示：请勿向不信任的反代端点传入 Cookie！）
@@ -60,88 +60,79 @@ recorderEvents = {
         })
 
         if (typeof sharedStorage !== 'undefined') {
-            message += `\n==========录播姬暂存数据部分==========\n录播姬暂存了${sharedStorage.length}个房间的真原画直播流地址}`;
+            message += `\n==========录播姬暂存数据部分==========\n录播姬暂存了${sharedStorage.length}个房间的真原画直播流地址`;
         }
 
         alert(message);
     },
     onFetchStreamUrl(data) {
-        const roomid = data.roomid;
-        let cache = null;
-        let qnArr = data.qn.length > 0 ? data.qn : [10000];
+        const roomid = data.roomid,
+            qnArr_def = [10000],
+            userQnArr = data.qn;
 
-        if (oldUrlSwitch && typeof sharedStorage !== 'undefined') {
-            cache = oldUrl(roomid, qnArr);
-            if (cache) {
-                return cache;
-            }
-        }
 
-        let playUrl = Fetch(roomid, 10000);
-
-        // @ts-ignore
+        // 向用户设置的 API 请求数据
+        let playUrl = urlFetch(roomid, qnArr_def[0]);
         if (!playUrl.ok) return null;
 
+
         // 直播间可选画质检测
+        const optionalQnCheck_cache = optionalQnCheck(userQnArr, playUrl);
+        let qnArr = [],
+            getQnArr = [];
+
+        qnArr = optionalQnCheck_cache.qnArr_Processed;
+        getQnArr = optionalQnCheck_cache.getQnArr;
+
         if (optionalQnCheckSwitch) {
-            cache = optionalQnCheck(qnArr, playUrl);
-            if (cache) {
-                console.log(cache.length);
-                if (cache.length < 1) {
-                    return 'http://0.0.0.0';
+            if (optionalQnCheck_cache.qnArr_Processed.length < 1) {
+                if (debugInfoShow) {
+                    console.log(`检测到没有符合设置要求的画质，将稍后再试。设置画质：[${userQnArr.toString()}]，可用画质：[${getQnArr.toString()}]`);
                 }
-
-                qnArr = cache;
-
-                // @ts-ignore
-                if (cache.forEach(x => x !== 10000 ? true : false)) {
-                    playUrl = Fetch(roomid, cache[0]);
-                    // @ts-ignore
-                    if (!playUrl.ok) return null;
-                }
-            } else {
-                return null;
+                return 'http://0.0.0.0';
             }
+
+            if (qnArr[0] !== qnArr_def[0]) {
+                playUrl = urlFetch(roomid, qnArr[0]);
+                if (!playUrl.ok) return null;
+            }
+
+        } else {
+            qnArr = qnArr_def;
         }
 
 
+        // JSON 处理
         /** @type {any[]?} */
-        // @ts-ignore
         const urls = JSON.parse(playUrl.body)?.data.durl?.map(x => x.url);
 
         if (!(urls?.length)) return null;
 
         const matchGotcha = /^https?\:\/\/[^\/]*ov-gotcha05\.bilivideo\.com\/.+$/;
-
         const filtered = urls.filter(x => matchGotcha.test(x));
 
         let playUrl_Processed = null;
-        if (filtered.length) {
-            playUrl_Processed = filtered[Math.floor(Math.random() * filtered.length)];
-        } else {
+        if (!filtered.length) {
             playUrl_Processed = urls[Math.floor(Math.random() * urls.length)];
         }
+        playUrl_Processed = filtered[Math.floor(Math.random() * filtered.length)];
 
 
         // 检测当前运行的录播姬的执行脚本内部是否存在 sharedStorage 方法，如果没有就使用获取的直播流地址
-        if (oldUrlSwitch) {
-            if (typeof sharedStorage !== 'undefined') {
-                // 检查是否是二压原画和非原画画质，不是的话保存
-                if (!(/_bluray/.test(playUrl_Processed)) && qnArr[0] === 10000) {
-                    if (debugInfoShow) {
-                        console.log("当前获取的直播流地址为真原画，已保存等待复用");
-                    }
-                    sharedStorage.setItem('playurl:room:' + roomid, playUrl_Processed);
-                } else {
-                    if (debugInfoShow) {
-                        console.warn("提示：当前获取的直播流地址为二压原画（地址中带有“_bluray”字样）或非原画画质（qn不是10000），故不对此次获取的直播流地址进行保存操作");
-                    }
-                }
-            } else {
-                if (debugInfoShow) {
-                    console.warn("提示：此录播姬的执行脚本内部不存在 sharedStorage 方法，因此无法进行旧直播流地址复用操作，将使用获取到的直播流地址");
-                }
+        if (!(oldUrlSwitch && typeof sharedStorage !== 'undefined')) {
+            if (debugInfoShow && typeof sharedStorage === 'undefined') {
+                console.warn("提示：此录播姬的执行脚本内部不存在 sharedStorage 方法，因此无法进行旧直播流地址复用操作，将使用获取到的直播流地址");
             }
+            if (debugInfoShow && oldUrlSwitch) {
+                console.log("由于“oldUrlSwitch”设置为了“false”，将直接输出直播流地址而不做暂存操作");
+            }
+
+            return playUrl_Processed;
+        }
+
+        let oldUrl_cache = oldUrl(roomid, playUrl_Processed, userQnArr, getQnArr, qnArr_def, qnArr);
+        if (oldUrl_cache) {
+            return oldUrl_cache;
         }
 
         return playUrl_Processed;
@@ -190,7 +181,7 @@ const timeStampConvert = (timeStamp) => {
 }
 
 // 封装好的请求方法
-const Fetch = (roomid, qn) => {
+const urlFetch = (roomid, qn) => {
     return fetchSync(`${FETCH_DOMAIN}/room/v1/Room/playUrl?cid=${roomid}&qn=${qn}&platform=web`, {
         method: 'GET',
         headers: {
@@ -203,30 +194,35 @@ const Fetch = (roomid, qn) => {
 }
 
 // 直播间可选画质检测
-const optionalQnCheck = (qnArr_Untreated, playUrl_Untreated) => {
+const optionalQnCheck = (userQnArr, playUrl_Untreated) => {
     let qnArr = [],
         getQnArr = JSON.parse(playUrl_Untreated.body)?.data.quality_description?.map(x => x.qn);
 
     if (getQnArr?.length) {
-        qnArr_Untreated.forEach(a => {
+        userQnArr.forEach(a =>
             getQnArr.forEach(b => {
                 if (a === b) {
                     qnArr.push(a);
                 }
             })
-        })
+        )
     }
-    return qnArr;
+
+    return {
+        qnArr_Processed: qnArr,
+        getQnArr: getQnArr
+    };
 }
 
 // 复用没有过期的真原画直播流地址
-const oldUrl = (roomid, qnArr) => {
+const oldUrl = (roomid, playUrl_Untreated, userQnArr, getQnArr, qnArr_def, qnArr) => {
     const oldUrl = sharedStorage.getItem('playurl:room:' + roomid),
         timeStamp = Date.now(),
-        qn = qnArr[0];
+        qn = userQnArr[0];
+    let playUrl_Processed = null;
 
     if (debugInfoShow) {
-        console.log(`选定录制画质：[${qnArr.toString()}]，最终选定录制画质：${qnArr[0]}`);
+        console.log(`用户选定的录制画质：[${userQnArr.toString()}]，当前直播间获取到的画质：[${getQnArr.toString()}]，最终选定录制画质：${qn}`);
         console.log(`当前获取的房间ID：${roomid}，选定录制的画质：${qn}(${qnConvert(qn)})，\n旧的直播流地址：${oldUrl ? oldUrl : "无"}`);
 
         if (qn !== 10000) {
@@ -235,7 +231,7 @@ const oldUrl = (roomid, qnArr) => {
     }
 
     if (oldUrl && qn === 10000) {
-        // 有旧的播放地址，检查是否过期
+        // 有旧的真原画直播流地址，检查是否过期
         const expires = Number(new URL(oldUrl).searchParams.get('expires'));
 
         if (debugInfoShow) {
@@ -244,21 +240,41 @@ const oldUrl = (roomid, qnArr) => {
 
         if ((timeStamp / 1000) + 10 < expires) {
             if (debugInfoShow) {
-                console.log("检测到当前获取的旧直播流地址未过期，使用旧直播流地址");
+                console.log("检测到当前获取的旧的真原画直播流地址未过期，使用旧直播流地址");
             }
 
-            return oldUrl;
+            playUrl_Processed = oldUrl;
         } else {
             if (debugInfoShow) {
-                console.log("检测到当前获取的旧直播流地址已过期，使用新的直播流地址");
+                console.log("检测到当前获取的旧的真原画直播流地址已过期，使用新的直播流地址");
             }
+
+            playUrl_Processed = playUrl_Untreated;
 
             sharedStorage.removeItem('playurl:room:' + roomid);
         }
 
     } else {
         if (debugInfoShow) {
-            console.log("未检测到当前录制房间存有旧的直播流地址，将使用新的直播流地址");
+            console.log("未检测到当前录制房间存有旧的真原画直播流地址，将使用新的直播流地址");
         }
     }
+
+    // 检查是否是二压原画和非原画画质，不是的话保存
+    if (!(/_bluray/.test(playUrl_Untreated)) && qn === qnArr_def[0]) {
+        if (debugInfoShow) {
+            console.log("当前获取的直播流地址为真原画，已保存等待复用");
+        }
+
+        if (oldUrl) {
+            sharedStorage.removeItem('playurl:room:' + roomid);
+        }
+        sharedStorage.setItem('playurl:room:' + roomid, playUrl_Untreated);
+    } else {
+        if (debugInfoShow) {
+            console.warn("提示：当前获取的直播流地址为二压原画（地址中带有“_bluray”字样）或非原画画质（qn不是10000），故不对此次获取的直播流地址进行保存操作");
+        }
+    }
+
+    return playUrl_Processed;
 }
