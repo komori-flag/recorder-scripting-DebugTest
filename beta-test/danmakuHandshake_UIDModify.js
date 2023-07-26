d778c704e050eff571ec57f28a121a4fd368ba1e
komori-flag <komoriwork@qq.com>
2023-07-27 00:38:38 +0800

    添加删除握手包当中的 key 和 platform 元素

--- beta-test/danmakuHandshake_UIDModify.js
+++ beta-test/danmakuHandshake_UIDModify.js
@@ -10,6 +10,9 @@ recorderEvents = {
         //             roomInfo.objectId: ${roomInfo.objectId},
         //             jsonData.uid: ${jsonData.uid},
         //             roomInfo.apiData.room_info.uid: ${roomInfo.apiData.room_info.uid}`)
+        // console.log(`弹幕握手包数据修改前：${json.toString()}`);
+        delete jsonData.key; delete jsonData.platform;
+        // console.log(`弹幕握手包数据修改后：${JSON.stringify(jsonData).toString()}`);
         jsonData.uid = roomInfo.apiData.room_info.uid;
         // console.log(`uid 修改后: ${jsonData.uid}`);
         return JSON.stringify(jsonData);
